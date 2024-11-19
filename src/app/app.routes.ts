2e9f745677ddeb9375dce70f89257b6c7fd85c89
Uziel <uzielcivil@gmail.com>
2024-11-19 16:58:47 -0300

    chore: defining actual routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { CreateAccFrameComponent } from './pages/authentication/create-acc-frame/create-acc-frame.component';
+import { LoginFrameComponent } from './pages/authentication/login-frame/login-frame.component';
+
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: 'login',
+  },
+  {
+    path: 'login',
+    component: LoginFrameComponent,
+  },
+  {
+    path: 'create',
+    component: CreateAccFrameComponent,
+  },
+];
