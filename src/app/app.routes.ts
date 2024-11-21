1ca06c780f01b7c05c7e0cc334cc4132cc6905a3
Uziel <uzielcivil@gmail.com>
2024-11-21 18:12:33 -0300

    chore: added guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,10 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
 
-import { CreateAccFrameComponent } from './pages/authentication/create-acc-frame/create-acc-frame.component';
-import { LoginFrameComponent } from './pages/authentication/login-frame/login-frame.component';
+import {CreateAccFrameComponent} from './pages/authentication/create-acc-frame/create-acc-frame.component';
+import {LoginFrameComponent} from './pages/authentication/login-frame/login-frame.component';
+import {NotFoundGuard} from "../shared/guards/not-found/not-found.guard";
+import {UserComponent} from "./pages/user/user/user.component";
+import {AuthGuard} from "../shared/guards/auth/auth.guard";
 
 export const routes: Routes = [
   {
@@ -17,4 +20,14 @@ export const routes: Routes = [
     path: 'create',
     component: CreateAccFrameComponent,
   },
+  {
+    path: 'user',
+    component: UserComponent,
+    canActivate: [AuthGuard],
+  },
+  {
+    path: '**',
+    component: LoginFrameComponent,
+    canActivate: [NotFoundGuard],
+  }
 ];
