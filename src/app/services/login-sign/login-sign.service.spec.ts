7743528c6bf35461f4cd9d11ddeb7e2d01b1f44d
Uziel <uzielcivil@gmail.com>
2024-11-23 12:23:36 -0300

    feat: addition of create methods and test update

--- src/app/services/login-sign/login-sign.service.spec.ts
+++ src/app/services/login-sign/login-sign.service.spec.ts
@@ -1,16 +1,80 @@
 import {TestBed} from '@angular/core/testing';
-
+import {HttpTestingController, provideHttpClientTesting} from '@angular/common/http/testing';
 import {LoginSignService} from './login-sign.service';
+import {LoginResponse} from '../../types/login.types';
 
 describe('LoginSignService', () => {
   let service: LoginSignService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        LoginSignService,
+        provideHttpClientTesting()
+      ]
+    });
     service = TestBed.inject(LoginSignService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should store auth token and username in session storage on login', () => {
+    const mockResponse: LoginResponse = {accessToken: 'fake-token', username: 'fake-user', expiresIn: 10000};
+    const formValue = {
+      email: 'test@example.com',
+      password: 'password'
+    }
+
+    service.login(formValue).subscribe(response => {
+      expect(response).toEqual(mockResponse);
+      expect(sessionStorage.getItem('auth-token')).toBe(mockResponse.accessToken);
+      expect(sessionStorage.getItem('username')).toBe(mockResponse.username);
+    });
+
+    const req = httpMock.expectOne('http://localhost:8080/auth/login');
+    expect(req.request.method).toBe('POST');
+    expect(req.request.body).toEqual(formValue);
+    req.flush(mockResponse);
+  });
+
+  it('should create an account', () => {
+    const mockSignupRequest = {username: 'newuser', email: 'newuser@example.com', password: 'newpassword'};
+    service.create(mockSignupRequest).subscribe(response => {
+      expect(response).toBeUndefined();
+    });
+    const createReq = httpMock.expectOne('http://localhost:8080/auth/create');
+    expect(createReq.request.method).toBe('POST');
+    expect(createReq.request.body).toEqual(mockSignupRequest);
+    createReq.flush({});
+  });
+
+  it('should create an account and log in', () => {
+    const mockSignupRequest = {username: 'mewuser', email: 'newuser@example.com', password: 'newpassword'};
+    const mockLoginResponse: LoginResponse = {
+      accessToken: 'new-fake-token',
+      username: 'new-fake-user',
+      expiresIn: 10000
+    };
+    service.createAndLogin(mockSignupRequest).subscribe(response => {
+      expect(response).toEqual(mockLoginResponse);
+      expect(sessionStorage.getItem('auth-token')).toBe(mockLoginResponse.accessToken);
+      expect(sessionStorage.getItem('username')).toBe(mockLoginResponse.username);
+    });
+    const createReq = httpMock.expectOne('http://localhost:8080/auth/create');
+    expect(createReq.request.method).toBe('POST');
+    expect(createReq.request.body).toEqual(mockSignupRequest);
+    createReq.flush({});
+    const loginReq = httpMock.expectOne('http://localhost:8080/auth/login');
+    expect(loginReq.request.method).toBe('POST');
+    expect(loginReq.request.body).toEqual({email: mockSignupRequest.email, password: mockSignupRequest.password,});
+    loginReq.flush(mockLoginResponse);
   });
 });
--- src/app/services/login-sign/login-sign.service.ts
+++ src/app/services/login-sign/login-sign.service.ts
@@ -1,7 +1,8 @@
 import {inject, Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable, tap} from "rxjs";
+import {Observable, switchMap, tap} from "rxjs";
 import {LoginResponse} from "../../types/login.types";
+import {LoginRequest, SignupRequest} from "../../types/form.types";
 
 const AUTH_URL = 'http://localhost:8080/auth';
 
@@ -10,13 +11,29 @@ const AUTH_URL = 'http://localhost:8080/auth';
 })
 export class LoginSignService {
 
-  http = inject(HttpClient);
+  private readonly http = inject(HttpClient);
 
 
-  login(email: string, password: string): Observable<LoginResponse> {
-    return this.http.post<LoginResponse>(AUTH_URL + '/login', {email, password}).pipe(tap((value) => {
+  login(formValue: LoginRequest): Observable<LoginResponse> {
+    return this.http.post<LoginResponse>(AUTH_URL + '/login', formValue).pipe(tap((value) => {
+      sessionStorage.setItem('auth-token', value.accessToken);
+      sessionStorage.setItem('username', value.username);
+    }))
+  }
+
+  create(formValue: SignupRequest): Observable<void> {
+    return this.http.post<void>(AUTH_URL + '/create', formValue)
+  }
+
+  createAndLogin(formValue: SignupRequest): Observable<LoginResponse> {
+    const loginRequest: LoginRequest = {
+      email: formValue.email,
+      password: formValue.password,
+    };
+    
+    return this.create(formValue).pipe(switchMap(() => this.login(loginRequest)), tap((value) => {
       sessionStorage.setItem('auth-token', value.accessToken);
       sessionStorage.setItem('username', value.accessToken);
-    }))
+    }));
   }
 }
