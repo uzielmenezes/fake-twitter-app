0ff02e90636080532927d3b12cb3863eee7d98e1
Uziel <uzielcivil@gmail.com>
2024-11-23 10:56:54 -0300

    chore: added the message service from primeng

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,18 @@
-import { provideHttpClient, withFetch } from '@angular/common/http';
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import {provideHttpClient, withFetch} from '@angular/common/http';
+import {ApplicationConfig, importProvidersFrom, provideZoneChangeDetection} from '@angular/core';
+import {BrowserAnimationsModule, provideAnimations} from '@angular/platform-browser/animations';
+import {provideRouter} from '@angular/router';
 
-import { routes } from './app.routes';
+import {routes} from './app.routes';
+import {MessageService} from "primeng/api";
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideZoneChangeDetection({eventCoalescing: true}),
     provideRouter(routes),
     provideHttpClient(withFetch()),
     provideAnimations(),
+    importProvidersFrom([BrowserAnimationsModule]),
+    MessageService,
   ],
 };
