3b0f28d5afbba0c8c9f253d1420def25852d66cf
Uziel <uzielcivil@gmail.com>
2024-11-23 14:08:48 -0300

    fix: wrong attribute call for username + duplicated code transformed into reusable function

--- src/app/services/login-sign/login-sign.service.ts
+++ src/app/services/login-sign/login-sign.service.ts
@@ -1,28 +1,26 @@
-import {inject, Injectable} from '@angular/core';
-import {HttpClient} from "@angular/common/http";
-import {Observable, switchMap, tap} from "rxjs";
-import {LoginResponse} from "../../types/login.types";
-import {LoginRequest, SignupRequest} from "../../types/form.types";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable, switchMap, tap } from 'rxjs';
+
+import { LoginRequest, SignupRequest } from '../../types/form.types';
+import { LoginResponse } from '../../types/login.types';
 
 const AUTH_URL = 'http://localhost:8080/auth';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LoginSignService {
-
   private readonly http = inject(HttpClient);
 
-
   login(formValue: LoginRequest): Observable<LoginResponse> {
-    return this.http.post<LoginResponse>(AUTH_URL + '/login', formValue).pipe(tap((value) => {
-      sessionStorage.setItem('auth-token', value.accessToken);
-      sessionStorage.setItem('username', value.username);
-    }))
+    return this.http
+      .post<LoginResponse>(AUTH_URL + '/login', formValue)
+      .pipe(tap((value) => this.setSessionStorage(value)));
   }
 
   create(formValue: SignupRequest): Observable<void> {
-    return this.http.post<void>(AUTH_URL + '/create', formValue)
+    return this.http.post<void>(AUTH_URL + '/create', formValue);
   }
 
   createAndLogin(formValue: SignupRequest): Observable<LoginResponse> {
@@ -30,10 +28,15 @@ export class LoginSignService {
       email: formValue.email,
       password: formValue.password,
     };
-    
-    return this.create(formValue).pipe(switchMap(() => this.login(loginRequest)), tap((value) => {
-      sessionStorage.setItem('auth-token', value.accessToken);
-      sessionStorage.setItem('username', value.accessToken);
-    }));
+
+    return this.create(formValue).pipe(
+      switchMap(() => this.login(loginRequest)),
+      tap((value) => this.setSessionStorage(value))
+    );
+  }
+
+  private setSessionStorage(value: LoginResponse) {
+    sessionStorage.setItem('auth-token', value.accessToken);
+    sessionStorage.setItem('username', value.username);
   }
 }
