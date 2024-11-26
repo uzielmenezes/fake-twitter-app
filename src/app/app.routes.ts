c139ef0a2e3142ebf5bff0431d61c504fea35b5e
Uziel <uzielcivil@gmail.com>
2024-11-26 09:53:57 -0300

    fix: useless folder removal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import {Routes} from '@angular/router';
 import {CreateAccFrameComponent} from './pages/authentication/create-acc-frame/create-acc-frame.component';
 import {LoginFrameComponent} from './pages/authentication/login-frame/login-frame.component';
 import {NotFoundGuard} from "../shared/guards/not-found/not-found.guard";
-import {UserComponent} from "./pages/user/user/user.component";
+import {UserComponent} from "./pages/user/user.component";
 import {AuthGuard} from "../shared/guards/auth/auth.guard";
 
 export const routes: Routes = [
