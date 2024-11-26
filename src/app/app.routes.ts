8981b04dc3cda82cc2f1b00e2f7f374d92017641
Uziel <uzielcivil@gmail.com>
2024-11-26 16:20:27 -0300

    feat: new pages feat after login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,10 @@ import {Routes} from '@angular/router';
 import {CreateAccFrameComponent} from './pages/authentication/create-acc-frame/create-acc-frame.component';
 import {LoginFrameComponent} from './pages/authentication/login-frame/login-frame.component';
 import {NotFoundGuard} from "../shared/guards/not-found/not-found.guard";
-import {HomeComponent} from "./pages/user/home.component";
+import {FakeTwitterComponent} from "./pages/fake-twitter/fake-twitter.component";
+import {MyTweetsComponent} from "./pages/fake-twitter/my-tweets/my-tweets.component";
+import {TweetsComponent} from "./pages/fake-twitter/tweets/tweets.component";
+import {AuthGuard} from "../shared/guards/auth/auth.guard";
 
 export const routes: Routes = [
   {
@@ -20,9 +23,14 @@ export const routes: Routes = [
     component: CreateAccFrameComponent,
   },
   {
-    path: 'home',
-    component: HomeComponent,
-    // canActivate: [AuthGuard],
+    path: 'fake-twitter',
+    component: FakeTwitterComponent,
+    children: [
+      {path: '', redirectTo: 'tweets', pathMatch: 'full'},
+      {path: 'tweets', component: TweetsComponent},
+      {path: 'my-tweets', component: MyTweetsComponent},
+    ],
+    canActivate: [AuthGuard],
   },
   {
     path: '**',
