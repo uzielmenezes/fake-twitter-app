b4b95e4ddab07fc7358be4272ce77d07b64fb72f
Uziel <uzielcivil@gmail.com>
2024-11-26 16:34:42 -0300

    feat: new tweets page

---
+++ src/app/pages/fake-twitter/tweets/tweets.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tweets',
+  standalone: true,
+  imports: [],
+  templateUrl: './tweets.component.html',
+  styleUrl: './tweets.component.scss'
+})
+export class TweetsComponent {
+
+}
