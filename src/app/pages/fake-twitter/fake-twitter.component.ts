19c5b999488e5f669f23cbde9387c3b64a6b808a
Uziel <uzielcivil@gmail.com>
2024-12-02 16:38:01 -0300

    feat: logoff dialog

--- src/app/pages/fake-twitter/fake-twitter.component.ts
+++ src/app/pages/fake-twitter/fake-twitter.component.ts
@@ -2,21 +2,27 @@ import {Component, OnInit} from '@angular/core';
 import {MenubarModule} from 'primeng/menubar';
 import {MenuItem} from "primeng/api";
 import {RouterOutlet} from "@angular/router";
+import {DialogModule} from "primeng/dialog";
+import {TitleCasePipe} from "@angular/common";
 
 @Component({
   selector: 'fake-twitter',
   standalone: true,
-  imports: [MenubarModule, RouterOutlet],
+  imports: [MenubarModule, RouterOutlet, DialogModule, TitleCasePipe],
   templateUrl: './fake-twitter.component.html',
   styleUrl: './fake-twitter.component.scss'
 })
 export class FakeTwitterComponent implements OnInit {
   items!: MenuItem[];
 
+  username!: string | null;
+
   initials!: string;
 
+  isVisible: boolean = false;
+
   ngOnInit() {
-    this.getUsernameInitials();
+    this.getUsernameAndInitials();
 
     this.items = [
       {
@@ -30,10 +36,19 @@ export class FakeTwitterComponent implements OnInit {
     ]
   }
 
-  private getUsernameInitials() {
-    let username = sessionStorage.getItem('username');
-    if (username) {
-      this.initials = username.split(' ').filter(name => name.length > 2).slice(0, 2).map(name => name.charAt(0).toUpperCase()).join('');
+  showDialog() {
+    this.isVisible = !this.isVisible;
+  }
+
+  onLogoff() {
+    sessionStorage.clear();
+    window.location.reload();
+  }
+
+  private getUsernameAndInitials() {
+    this.username = sessionStorage.getItem('username');
+    if (this.username) {
+      this.initials = this.username.split(' ').filter(name => name.length > 2).slice(0, 2).map(name => name.charAt(0).toUpperCase()).join('');
     }
   }
 }
